df8cfc1c2895d2b04b1608d9d95b3326576d3abd
CarlaMendoza90 <mendozacoronadocarla@gmail.com>
2025-07-24 19:12:51 -0300

    [NUEVO] Ahora podemos obtener productos por id

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,4 +1,10 @@
 import * as Model from "../models/products.model.js"
+
 export const getAllProducts = async () => {
     return await Model.getAllProducts();
 };
+
+export const getProductById = async (id) => {
+    return await Model.getProductById(id);
+};
+
