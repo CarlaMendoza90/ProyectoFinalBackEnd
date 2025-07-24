613502a5d6a983cfb5de2dd8971751be35a36e8c
CarlaMendoza90 <mendozacoronadocarla@gmail.com>
2025-07-24 20:03:42 -0300

    [NUEVO] Token de autorizacion

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json({message: "Bienvenidos a la API Rest"});
 });
@@ -10,5 +12,8 @@ const PORT = process.env.PORT || 3001;
 
 import productsRouter from "./src/routes/products.router.js";
 app.use("/api", productsRouter);
+
+import authRouter from "./src/routes/auth.router.js";
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
