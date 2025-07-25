4e826ae77bf04ccd54687c3d026202e84379a45a
CarlaMendoza90 <mendozacoronadocarla@gmail.com>
2025-07-24 22:42:23 -0300

    [NUEVO] Cors y 404 en ruta desconocida.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -15,5 +17,9 @@ app.use("/api", productsRouter);
 
 import authRouter from "./src/routes/auth.router.js";
 app.use("/api/auth", authRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send("Recurso no encontrado o ruta invalida.");
+})
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
